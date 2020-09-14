f11549a182ae57eac129cf83a6b1b3ce2f342101
Jonas Haneborg <jon.haneborg@gmail.com>
2020-09-14 21:22:32 +0200

    Small corrections.

--- src/main/java/no/hvl/dat250/jpa/classes/Main.java
+++ src/main/java/no/hvl/dat250/jpa/classes/Main.java
@@ -1,4 +1,4 @@
-package no.hvl.dat250.jpa.basicexample;
+package no.hvl.dat250.jpa.classes;
 
 import java.util.List;
 
--- src/main/java/no/hvl/dat250/jpa/classes/User.java
+++ src/main/java/no/hvl/dat250/jpa/classes/User.java
@@ -1,4 +1,4 @@
-package no.hvl.dat250.jpa.basicexample;
+package no.hvl.dat250.jpa.classes;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
